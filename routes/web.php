62117c001edfbc01497674fb15a479a31fa1e961
AlwanAlawi <alwanalawi1@gmail.com>
2023-02-20 09:25:47 +0700

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// A
-Route::get('/', function () {
-    return "Selamat Pagi";
-});
-
-// B
-Route::get('/about', function () {
-    return "Nama : Alwan Aawi Nim : 2141720178";
-});
-
-// C
-Route::get('/articles/{id}', function ($id) {
-    return "Halaman Artikel dengan ID $id";
-});
